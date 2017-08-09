14b4a7806ca667608b367b6aa3650d3db2f850a7
Maisie Campbell <maisie-campbell@hotmail.com>
2017-08-09 12:33:01 +0100

    removed slash as it is unnecessary

--- reusable_blog/urls.py
+++ reusable_blog/urls.py
@@ -19,7 +19,7 @@ import views
 urlpatterns = [
 
     url(r'^$', views.post_list, name="post_list"),
-    url(r'^/stuff/$', views.post_list, name="post_list"),
+    url(r'^stuff/$', views.post_list, name="post_list"),
     url(r'^(?P<id>\d+)/$', views.post_detail),
     url(r'^post/$', views.new_post, name='new_post'),
 
